d42cb270507ee742dac9ac14421c9d6dbd0fee5a
AshwinBalaji <AshwinBalaji@ASHWINBALAJI-PC>
2018-07-28 20:57:52 +0530

    Assembly info updated

--- FirstWindowsFormsApplication/FirstWindowsFormsApplication/Properties/AssemblyInfo.cs
+++ FirstWindowsFormsApplication/FirstWindowsFormsApplication/Properties/AssemblyInfo.cs
@@ -22,15 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0dc41cdb-3d5e-4147-b409-b2184724399b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.*")]
+ 
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
